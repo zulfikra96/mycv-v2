3bdd00ea29b5490e351c0a4c5581b88d206ec8b6
zulfikra96 <zulfikralahmudin@gmail.com>
2024-03-04 13:53:54 +0800

    add bundle css

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod config;
 mod controllers;
 mod routes;
 mod models;
-
+use std::{io::{BufReader, Read, Write}, path::Path};
 use actix::Actor;
 use actix_session::{SessionMiddleware, storage::CookieSessionStore};
 use config::socket_server;
@@ -13,6 +13,14 @@ use actix_web::{cookie::Key, http, middleware, web, App, HttpServer};
 use controllers::home;
 use diesel::{r2d2::{ConnectionManager, Pool}, PgConnection};
 use dotenv::dotenv;
+use std::fs as file_system;
+use lightningcss::{
+    bundler::{Bundler, FileProvider}, printer::PrinterOptions, rules::style, stylesheet::{MinifyOptions, ParserOptions, StyleSheet}
+};
+
+
+
+
 
 pub type DBPool = Pool<ConnectionManager<PgConnection>>;
 
@@ -22,7 +30,21 @@ async fn main() -> std::io::Result<()> {
     let port = std::env::var("PORT")
         .expect("Port is not defined").parse::<u16>().unwrap();
     let server = socket_server::ChatServer::default().start();
-    
+    let path: &Path = Path::new("public/css/style.css");
+    // let display = path.display();
+
+    // let fs = FileProvider::new();
+    // let mut style = file_system::File::open(&path).unwrap();
+    // let mut s = String::new();
+    // style.read_to_string(&mut s).unwrap();
+    // let mut stylesheet = StyleSheet::parse(&s, ParserOptions::default()).unwrap();
+    // stylesheet.minify(MinifyOptions::default()).unwrap();
+    // let res = stylesheet.to_css(PrinterOptions {
+    //     minify: true, 
+    //     ..PrinterOptions::default()
+    // }).unwrap();
+    // let mut f = file_system::File::create("public/css/bundle-style.min.css").unwrap();
+    // f.write_all(res.code.as_bytes());
     HttpServer::new(move || {
         let cors = Cors::default()
             .allowed_origin("http://localhost:3000")
